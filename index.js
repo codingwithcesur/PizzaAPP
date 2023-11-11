bb665e7e8bf4ac3a95aa7c40a7fe3a372aefd05c
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-11 13:47:26 +0300

    import middlewares to index & add home route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
@@ -9,43 +9,57 @@
     $ npm i express dotenv mongoose express-async-errors
     $ npm i jsonwebtoken morgan
 */
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 /* ------------------------------------------------------- */
 // Required Modules:
 
 // envVariables to process.env:
-require('dotenv').config()
-const PORT = process.env?.PORT || 8000
+require("dotenv").config();
+const PORT = process.env?.PORT || 8000;
 
 // asyncErrors to errorHandler:
-require('express-async-errors')
+require("express-async-errors");
 
 /* ------------------------------------------------------- */
 // Configrations:
 
 // Connect to DB:
-const { dbConnection } = require('./src/configs/dbConnection')
-dbConnection()
+const { dbConnection } = require("./src/configs/dbConnection");
+dbConnection();
 
 /* ------------------------------------------------------- */
 // Middlewares:
 
+// Accept JSON:
+app.use(express.json());
 
+// Logger:
+app.use(require("./src/middlewares/logger"));
+
+// Find Search Sort Page:
+app.use(require("./src/middlewares/findSearchSortPage"));
 
 /* ------------------------------------------------------- */
 // Routes:
 
-
-
+// Home:
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Pizza API",
+    isLogin: req.isLogin,
+    user: req.user,
+  });
+});
 /* ------------------------------------------------------- */
 
 // errorHandler:
-app.use(require('./src/middlewares/errorHandler'))
+app.use(require("./src/middlewares/errorHandler"));
 
 // RUN SERVER:
-app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+app.listen(PORT, () => console.log("http://127.0.0.1:" + PORT));
 
 /* ------------------------------------------------------- */
 // Syncronization (must be in commentLine):
