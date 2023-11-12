ba407538a42f3df8bf64b1f82a1e0e77413d7cdb
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-12 16:29:06 +0300

    add order controller

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,4 +1,76 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
+// Pizza Controller:
+const Pizza = require("../models/order");
+
+module.exports = {
+  list: async (req, res) => {
+    /*
+            #swagger.tags = ["Order"]
+            #swagger.summary = "List Order"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+    const data = await res.getModelList(Pizza);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Pizza),
+      data,
+    });
+  },
+  create: async (req, res) => {
+    /*
+            #swagger.tags = ["Order"]
+            #swagger.summary = "Create Pizza"
+        */
+
+    const data = await Pizza.create(req.body);
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
+  read: async (req, res) => {
+    /*
+            #swagger.tags = ["Order"]
+            #swagger.summary = "Get Single Pizza"
+        */
+    const data = await Pizza.findOne({ _id: req.params.id });
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Pizza),
+      data,
+    });
+  },
+  update: async (req, res) => {
+    /*
+            #swagger.tags = ["Order"]
+            #swagger.summary = "Update Pizza"
+        */
+    const data = await Pizza.updateOne({ _id: req.params.id }, req.body);
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Pizza.findOne({ _id: req.params.id }),
+    });
+  },
+  delete: async (req, res) => {
+    /*
+            #swagger.tags = ["Order"]
+            #swagger.summary = "Delete Pizza"
+        */
+    const data = await Pizza.deleteOne({ _id: req.params.id });
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
