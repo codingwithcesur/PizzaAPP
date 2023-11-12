1e59f13e5a8b737c64951ccb3f46cef61b64e074
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-12 16:26:57 +0300

    add order model

--- src/models/order.js
+++ src/models/order.js
@@ -1,6 +1,45 @@
-"use strict"
+"use strict";
+const e = require("express");
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Order Model:
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    pizzaId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Pizza",
+      required: true,
+    },
+    size: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: ["Small", "Medium", "Large"],
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantitiy: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+    },
+  },
+  { collection: "orders", timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
