a37f09928a86c4d105bf5e68f10670e96ef9f8f9
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-12 16:18:17 +0300

    add pizza model

--- src/models/pizza.js
+++ src/models/pizza.js
@@ -1,6 +1,38 @@
-"use strict"
+"use strict";
+const e = require("express");
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Pizza Model:
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const PizzaSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    toppings: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Topping",
+      },
+    ],
+  },
+  { collection: "pizzas", timestamps: true }
+);
+
+module.exports = mongoose.model("Pizza", PizzaSchema);
