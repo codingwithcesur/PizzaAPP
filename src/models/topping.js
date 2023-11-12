6b00be7978060cc60f7f00a87755c938f6ee136a
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-11-12 16:10:02 +0300

    add toppings model

--- src/models/topping.js
+++ src/models/topping.js
@@ -1,6 +1,24 @@
-"use strict"
+"use strict";
+const e = require("express");
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Topping Model:
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+const ToppingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+  },
+  { collection: "toppings", timestamps: true }
+);
+
+module.exports = mongoose.model("Topping", ToppingSchema);
